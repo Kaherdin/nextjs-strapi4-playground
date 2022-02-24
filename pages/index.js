88aee8e761358da3b161dbc2dff2e9952f83bc08
Kaherdin <aurelien.borst@gmail.com>
2022-02-24 14:10:41 +0100

    fetch data with gql

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import Head from "next/head";
-import Image from "next/image";
+import { useQuery } from "@apollo/client";
 import styles from "../styles/Home.module.css";
+import { gql } from "@apollo/client";
+
+const POSTS = gql`
+  query posts {
+    posts(pagination: { start: 0, limit: 2 }) {
+      data {
+        id
+        attributes {
+          title
+          text
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
+  const { data, fetchMore } = useQuery(POSTS, {
+    variables: { start: 0, limit: 2 },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  console.log(data, "data");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +35,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to this playground</h1>
+        <ul>
+          {data?.posts?.data?.map((post, index) => {
+            return <li key={index}>{post?.attributes?.title}</li>;
+          })}
+        </ul>
       </main>
     </div>
   );
